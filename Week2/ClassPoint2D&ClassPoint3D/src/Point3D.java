362c9b1d9cb0d6003d1f41206adff08ca7bb222c
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-06-16 14:49:52 +0700

    Point

---
+++ Week2/ClassPoint2D&ClassPoint3D/src/Point3D.java
@@ -0,0 +1,43 @@
+public class Point3D extends Point2D {
+    public float z = 0.0f;
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public Point3D() {
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public float[] getXYZ() {
+        float[] array = new float[3];
+        array[1] = x;
+        array[2] = y;
+        array[3] = z;
+        return array;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                "z=" + z +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
