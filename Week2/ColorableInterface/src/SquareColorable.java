b8e7ed08c1118539cc5e2e8fe80e7db5e9d39d5c
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-06-16 22:13:02 +0700

    Colorable

---
+++ Week2/ColorableInterface/src/SquareColorable.java
@@ -0,0 +1,29 @@
+public class SquareColorable {
+    public static void main(String[] args) {
+        Shape[] shape = new Shape[10];
+        for (int i = 0; i < shape.length; i++) {
+            shape[i] = getRandomShape();
+        }
+        Shape.printShape(shape);
+        for (Shape x : shape) {
+            if (x instanceof Colorable) {
+                ((Colorable) x).howToColor();
+            }
+        }
+
+    }
+
+    static Shape getRandomShape() {
+        int random = (int) (Math.round(Math.random() * 2));
+        switch (random) {
+            case 0:
+                return new Square((double) (Math.round(Math.random() * 100)));
+            case 1:
+                return new Rectangle((double) (Math.round(Math.random() * 100)), (double) (Math.round(Math.random() * 100)));
+            default:
+                return new Circle((double) (Math.round(Math.random() * 100)));
+        }
+    }
+
+
+}
