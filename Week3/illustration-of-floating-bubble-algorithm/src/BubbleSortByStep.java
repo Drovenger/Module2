7fa3d01d6fa799dbc8c4f438fe1ce078d59d926c
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-06-25 17:06:02 +0700

    illustration-of-floating-bubble-algorithm

--- Week3/illustration-of-floating-bubble-algorithm/src/BubbleSortByStep.java
+++ Week3/illustration-of-floating-bubble-algorithm/src/BubbleSortByStep.java
@@ -11,11 +11,12 @@ public class BubbleSortByStep {
             list[i] = (int) (Math.random() * 100) + 1;
         }
         System.out.println("Your input list: ");
-        for (int i = 0; i < list.length; i++) {
-            System.out.print(list[i] + "\t");
+        for (int value : list) {
+            System.out.print(value + "\t");
         }
         System.out.println("\nBegin sort processing...");
-        bubbleSortByStep(list);
+        //bubbleSortByStep(list);
+        quickSort(list, 0, list.length - 1);
     }
 
     public static void bubbleSortByStep(int[] list) {
@@ -34,16 +35,60 @@ public class BubbleSortByStep {
                 }
             }
             /* Array may be sorted and next pass not needed */
-            if (needNextPass == false) {
+            if (!needNextPass) {
                 System.out.println("Array may be sorted and next pass not needed");
                 break;
             }
             /* Show the list after sort */
             System.out.print("List after the  " + k + "' sort: ");
-            for (int j = 0; j < list.length; j++) {
-                System.out.print(list[j] + "\t");
+            for (int i : list) {
+                System.out.print(i + "\t");
             }
             System.out.println();
         }
+    }
+
+    public static void insertionSort(int[] list) {
+        for (int i = 1; i < list.length; i++) {
+            //insert list[i] into a sorted sublist list[0...i-1]
+            //so that list[0..i] is sorted
+            int currentElement = list[i];
+            int k;
+            for (k = i - 1; k >= 0 && list[k] > currentElement; k--) {
+                list[k + 1] = list[k];
+            }
+            //insert the current element into list[k+1]
+            list[k + 1] = currentElement;
+        }
+    }
+
+    public static void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    public static int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i + 1;
     }
 }
