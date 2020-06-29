de1dac939e635145388f4023e46a6ab96e526e01
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-06-29 10:02:56 +0700

    update

--- Week3/check-out-the-Palindrome-string-using-Queue/src/Palindrome.java
+++ Week3/check-out-the-Palindrome-string-using-Queue/src/Palindrome.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Palindrome {
+    public static void main(String[] args) {
+        System.out.print("Enter any string: ");
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        inputString = inputString.toLowerCase();
+        Queue queue = new LinkedList();
+        for (int i = inputString.length() - 1; i >= 0; i--) {
+            queue.add(inputString.charAt(i));
+        }
+
+        String reverseStringResult = "";
+
+        while (!queue.isEmpty()) {
+            reverseStringResult = reverseStringResult + queue.remove();
+        }
+
+        if (inputString.equals(reverseStringResult))
+            System.out.println("The input String is a Palindrome");
+        else
+            System.out.println("The iput String is not a Palindrome");
+    }
 }
