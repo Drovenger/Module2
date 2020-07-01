e9414c074addb50acf2b4d3aa4a2759ee6f0953a
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-07-01 09:36:21 +0700

    read-file-csv

--- Week4/read-file-csv/src/CVSReader2.java
+++ Week4/read-file-csv/src/CVSReader2.java
@@ -1,3 +1,21 @@
-public class CVSReader2 {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class CVSReader2 {
+    public static void main(String[] args) {
+        String csvFile = "src/country.csv";
+        String line = "";
+        String csvSplitBy = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            while ((line = br.readLine()) != null) {
+                //use comma as separator
+                String[] country = line.split(csvSplitBy);
+                System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
